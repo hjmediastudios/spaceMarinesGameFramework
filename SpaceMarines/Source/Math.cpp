d87786300b33f303395186a7ce9640da411df7ad
Nick Royer <hjmediastudios@gmail.com>
2012-11-15 13:32:29 -0600

    Implemented FXAA antialiasing

---
+++ SpaceMarines/Source/Math.cpp
@@ -0,0 +1,12 @@
+#include "Math.hpp"
+
+namespace SpaceMarines
+{
+
+const Vector3 Vector3::ONE = Vector3(1.0f, 1.0f, 1.0f);
+const Vector3 Vector3::ZERO = Vector3(0.0f, 0.0f, 0.0f);
+const Vector3 Vector3::UP = Vector3(0.0f, 1.0f, 0.0f);
+const Vector3 Vector3::FORWARD = Vector3(0.0f, 0.0f, 1.0f);
+const Vector3 Vector3::RIGHT = Vector3(1.0f, 0.0f, 0.0f);
+
+}
